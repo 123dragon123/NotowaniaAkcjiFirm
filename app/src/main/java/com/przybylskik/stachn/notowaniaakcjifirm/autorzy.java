1244496aee2d92be6a86353b3eb02b2fe15446a9
ogryz <ogryz.rules@gmail.com>
2017-05-26 00:00:30 +0200

    Utworzono aktywność autorzy, dodano w manifescie dostęp do internetu oraz kolejną aktywność

---
+++ app/src/main/java/com/przybylskik/stachn/notowaniaakcjifirm/autorzy.java
@@ -0,0 +1,14 @@
+package com.przybylskik.stachn.notowaniaakcjifirm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class autorzy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autorzy);
+    }
+
+}
