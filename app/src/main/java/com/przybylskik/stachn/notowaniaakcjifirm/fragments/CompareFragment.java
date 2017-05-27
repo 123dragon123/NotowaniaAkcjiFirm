0da5cf8917035af638f727c1fceba007c60bb89b
ogryz <ogryz.rules@gmail.com>
2017-05-27 23:04:57 +0200

    Poprawa aktwyności compare oraz wyświetlania layouta

--- app/src/main/java/com/przybylskik/stachn/notowaniaakcjifirm/fragments/CompareFragment.java
+++ app/src/main/java/com/przybylskik/stachn/notowaniaakcjifirm/fragments/CompareFragment.java
@@ -1,19 +1,32 @@
-package com.przybylskik.stachn.notowaniaakcjifirm.fragments;
+package com.example.przybylskik.stachn.notowaniaakcjifirm.fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.constraint.ConstraintLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.przybylskik.stachn.notowaniaakcjifirm.R;
+import com.example.przybylskik.stachn.notowaniaakcjifirm.CompareStockData;
+import com.example.przybylskik.stachn.notowaniaakcjifirm.R;
 
-public class CompareFragment extends android.app.Fragment {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    public CompareFragment() {
-        // Required empty public constructor
+public class CompareFragment extends android.app.Fragment implements View.OnClickListener{
+
+    //Tworzymy obiekty GIU
+    EditText editTextNazwaFirmy1, editTextNazwaFirmy2;
+    EditText editTextFirma1DD, editTextFirma1MM, editTextFirma1RRRR;
+    EditText editTextFirma2DD, editTextFirma2MM, editTextFirma2RRRR;
+    Button buttonPorownajDane;
+
+    public CompareFragment()
+    {
+
     }
 
     @Override
@@ -22,14 +35,77 @@ public class CompareFragment extends android.app.Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compare, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        //Inicjalizacja obiektów GUI
+        ConstraintLayout cl = (ConstraintLayout)inflater.inflate(R.layout.fragment_compare, container, false);
+
+        editTextNazwaFirmy1 = (EditText)cl.findViewById(R.id.editTextNazwaFirmy1);
+        editTextFirma1DD = (EditText)cl.findViewById(R.id.editTextFirma1DD);
+        editTextFirma1MM = (EditText)cl.findViewById(R.id.editTextFirma1MM);
+        editTextFirma1RRRR = (EditText)cl.findViewById(R.id.editTextFirma1RRRR);
+
+        editTextNazwaFirmy2 = (EditText)cl.findViewById(R.id.editTextNazwaFirmy2);
+        editTextFirma2DD = (EditText)cl.findViewById(R.id.editTextFirma2DD);
+        editTextFirma2MM = (EditText)cl.findViewById(R.id.editTextFirma2MM);
+        editTextFirma2RRRR = (EditText)cl.findViewById(R.id.editTextFirma2RRRR);
+
+        buttonPorownajDane = (Button)cl.findViewById(R.id.buttonPorownajDane);
+        buttonPorownajDane.setOnClickListener(this);
+
+        return cl;
     }
 
     @Override
-    public void onDetach() {
+    public void onDetach()
+    {
         super.onDetach();
+    }
+
+    @Override
+    public void onClick(View v) //W przypadku nacisnięcia"Porównaj dane"
+    {
+        switch(v.getId())
+        {
+            case R.id.buttonPorownajDane:
+            {
+                //Zapisujemy dane podane przez użytkownika w zmiennt
+                String nazwaFirmy1 = editTextNazwaFirmy1.getText().toString();
+                String nazwaFirmy2 = editTextNazwaFirmy2.getText().toString();
+
+                String firma1Data = editTextFirma1RRRR.getText().toString()+"-"+editTextFirma1MM.getText().toString()+"-"+editTextFirma1DD.getText().toString();
+                String firma2Data = editTextFirma2RRRR.getText().toString()+"-"+editTextFirma2MM.getText().toString()+"-"+editTextFirma2DD.getText().toString();
+
+                try
+                {
+                    //------Sprawdzamy czy data jest poprawna
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    sdf.setLenient(false);
+                    Date date;
+                    date = sdf.parse(firma1Data);
+                    date = sdf.parse(firma2Data);
+                    //------
+
+                    //Przekazujemy dane do aktywności CompareStockData
+                    Intent intent = new Intent(this.getActivity(), CompareStockData.class);
+                    intent.putExtra("nazwaFirmy1", nazwaFirmy1);
+                    intent.putExtra("nazwaFirmy2",nazwaFirmy2);
+                    intent.putExtra("data1", firma1Data);
+                    intent.putExtra("data2", firma2Data);
+
+                    startActivity(intent);
+
+                }
+                catch (java.text.ParseException e)
+                {
+                    Toast.makeText(this.getActivity(), "Niepoprawna data", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
     }
 }
