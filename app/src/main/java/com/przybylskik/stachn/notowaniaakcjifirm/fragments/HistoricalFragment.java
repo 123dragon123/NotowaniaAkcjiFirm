d6c58d87e6b0375160b2564b92285ba4503cd752
123dragon123 <snatalia17@onet.eu>
2017-05-27 18:57:04 +0200

    Dodano fragment, który odpowiada za pobieranie danych historycznych o notowaniach akcji firm z sieci. Przy niedostępności danych na wskazaną date uzytkownik otrzymuje odpowiedni komunikat. Zaprojektowano interfejs fragmentu HistoricalFragment w pliku fragment_historical.xml

--- app/src/main/java/com/przybylskik/stachn/notowaniaakcjifirm/fragments/HistoricalFragment.java
+++ app/src/main/java/com/przybylskik/stachn/notowaniaakcjifirm/fragments/HistoricalFragment.java
@@ -1,35 +1,97 @@
 package com.przybylskik.stachn.notowaniaakcjifirm.fragments;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.constraint.ConstraintLayout;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Toast;
 
-import com.przybylskik.stachn.notowaniaakcjifirm.R;
+        import com.przybylskik.stachn.notowaniaakcjifirm.GetStocksData;
+        import com.przybylskik.stachn.notowaniaakcjifirm.R;
 
-public class HistoricalFragment extends android.app.Fragment {
+        import java.text.SimpleDateFormat;
+        import java.util.Date;
 
-    public HistoricalFragment() {
-        // Required empty public constructor
+public class HistoricalFragment extends android.app.Fragment implements View.OnClickListener
+{
+    //------Towrzymy obiekty interfejsu użytkownika
+    EditText editTextDzien, editTextMiesiac, editTextRok, editTextNazwaFirmy;
+    Button buttonPobierzDane;
+    //------
+
+    public HistoricalFragment()
+    {
+
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_historical, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        //------Inicjalizacja obiektów interfejsu użytkownika
+        ConstraintLayout cl = (ConstraintLayout)inflater.inflate(R.layout.fragment_historical, container, false);
+        buttonPobierzDane = (Button)cl.findViewById(R.id.buttonPobierzDane);
+        buttonPobierzDane.setOnClickListener(this);
+
+        editTextDzien = (EditText)cl.findViewById(R.id.editTextDzien);
+        editTextMiesiac = (EditText)cl.findViewById(R.id.editTextMiesiac);
+        editTextRok = (EditText)cl.findViewById(R.id.editTextRok);
+        editTextNazwaFirmy = (EditText)cl.findViewById(R.id.editTextNazwaFirmy);
+        //------
+
+        return cl;
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    @Override
+    public void onClick(View v)//W przypadku kiedy naciśnięto
+    {
+        switch(v.getId())
+        {
+            case R.id.buttonPobierzDane: //Pobierz dane
+            {
+                String data = editTextRok.getText().toString()+"-"+editTextMiesiac.getText().toString()+"-"+editTextDzien.getText().toString();//Zapisujemy datę do zmiennej
+                try
+                {
+                    //------Ten kod sprawdza, czy istnieje taka data
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    sdf.setLenient(false);
+                    Date date = sdf.parse(data);
+                    //------
+
+                    //------Jeżeli istnieje to przekazuje dane do aktywności GetStockData
+                    Intent intent = new Intent(this.getActivity(), GetStocksData.class);
+                    intent.putExtra("nazwaFirmy", editTextNazwaFirmy.getText().toString());
+                    intent.putExtra("dataStart", data);
+                    intent.putExtra("dataEnd", data);
+
+                    startActivity(intent);
+                }
+                catch (java.text.ParseException e)//Jeżeli data wskazana niepoprawnie to otrzymujemy Exception
+                {
+                    Toast.makeText(this.getActivity(), "Niepoprawna data", Toast.LENGTH_SHORT).show();//Wyświetlamy odowiedni komunikat
+                }
+                break;
+            }
+
+            default:
+            {
+
+            }
+        }
     }
 }
